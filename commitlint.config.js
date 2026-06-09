65d8bd8ec0b68d4338e3ec6d27940834d8a7f1a2
QHung1209 <21522117@gm.uit.edu.vn>
2026-06-09 15:57:50 +0700

    chore/#commitLint: custom configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,43 @@
-export default { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+
+  parserPreset: {
+    parserOpts: {
+      // Format: <type>/#<scope>: <description>
+      headerPattern: /^(\w*)\/#(\w*): (.*)$/,
+
+      headerCorrespondence: ['type', 'scope', 'subject'],
+    },
+  },
+
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+      ],
+    ],
+
+    'header-min-length': [2, 'always', 10],
+
+    'header-max-length': [2, 'always', 160],
+
+    'body-max-line-length': [2, 'always', 120],
+
+    'subject-case': [
+      0,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+  },
+};
